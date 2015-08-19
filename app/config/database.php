35976bb8030f1a9b64f2e0cac0f131b3cf7f4f1c
TechyTimo <techytimo@gmail.com>
2015-08-19 10:05:34 +0300

    removed keys from source

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'thedevs.org',
-			'username'  => 'root',
-			'password'  => 'ttunawesmake',
+			'database'  => '',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
